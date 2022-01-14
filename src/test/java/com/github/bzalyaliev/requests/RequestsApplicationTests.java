a0152b60023ad5c123ca8243739a46292eaa974e
Bulat Zalyaliev <bzalyaliev@gmail.com>
2022-01-14 16:09:06 +0300

    Initial commit

---
+++ src/test/java/com/github/bzalyaliev/requests/RequestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bzalyaliev.requests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
