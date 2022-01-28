2cb7ac2eb564913dbfa8b2a58c51dad6ee059459
artemptushkin <artem.ptushkin@ev-box.com>
2022-01-28 11:51:22 +0100

    FE runs inside the jar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-import {Link, Route, Routes} from "react-router-dom";
-import Home from "./Home";
+import {Link} from "react-router-dom";
 import {Component} from "react";
 
 class App extends Component {
-  render() {
-    return (
-        <div className="App">
-            <div className="menu">
-                <ul>
-                    <li> <Link to="/request">Новый запрос</Link></li>
-                    <li> <Link to="/requests">Все Запросы</Link></li>
-                </ul>
+    render() {
+        return (
+            <div className="App">
+                <div className="menu">
+                    <ul>
+                        <li><Link to="/request">Новый запрос</Link></li>
+                        <li><Link to="/requests">Все Запросы</Link></li>
+                    </ul>
+                </div>
+                <div className="App-intro">
+                    {/*  <Route path={'/request/:id'} component={RequestPage}/>
+                        <Route path={'/request'} exact={true} component={NewRequestPage}/>
+                        <Route path={'/requests'} exact={true} component={RequestsPage}/>*/}
+                </div>
             </div>
-            <div className="App-intro">
-                <Routes>
-                  {/*  <Route path={'/request/:id'} component={RequestPage}/>
-                    <Route path={'/request'} exact={true} component={NewRequestPage}/>
-                    <Route path={'/requests'} exact={true} component={RequestsPage}/>*/}
-                </Routes>
-            </div>
-        </div>
-    )
-  };
+        )
+    };
 }
 
 export default App;
