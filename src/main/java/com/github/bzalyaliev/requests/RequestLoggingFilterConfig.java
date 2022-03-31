0f2d2988f403746fd2179d549806b1dce4236e94
bzalyaliev <bzalyaliev@gmail.com>
2022-03-31 15:59:56 +0300

    logback-spring.xml file added. Logging filter added.

---
+++ src/main/java/com/github/bzalyaliev/requests/RequestLoggingFilterConfig.java
@@ -0,0 +1,20 @@
+package com.github.bzalyaliev.requests;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter
+                = new CommonsRequestLoggingFilter();
+        filter.setIncludeClientInfo(true);
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(2000);
+        return filter;
+    }
+}
