21f9ebdbcc60fa389246747a9ef075c060b36fd4
bzalyaliev <bzalyaliev@gmail.com>
2022-06-17 13:53:49 +0300

    itSavesUser() added checking role. itShouldNotSavesUserWithRoleThatDoesNotExistsInDatabase() test added

--- src/test/java/com/github/bzalyaliev/requests/UserRepositoryTest.java
+++ src/test/java/com/github/bzalyaliev/requests/UserRepositoryTest.java
@@ -8,11 +8,13 @@ import com.github.bzalyaliev.requests.repository.entity.UserEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.ZonedDateTime;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -42,17 +44,54 @@ public class UserRepositoryTest {
                 .build();
 
         UserEntity savedUserEntity = userRepository.save(userEntity);
-
-        Optional<RoleEntity> roleEntityFromRepository = roleRepository.findById(savedRoleEntity.getId());
         Optional<UserEntity> userEntityFromRepository = userRepository.findById(savedUserEntity.getId());
-
-        assertThat(roleEntityFromRepository).isPresent();
-        assertThat(roleEntityFromRepository.get().getId()).isEqualTo(savedRoleEntity.getId());
-        assertThat(roleEntityFromRepository.get().getName()).isEqualTo(savedRoleEntity.getName());
 
         assertThat(userEntityFromRepository).isPresent();
         assertThat(userEntityFromRepository.get().getId()).isEqualTo(savedUserEntity.getId());
         assertThat(userEntityFromRepository.get().getLogin()).isEqualTo(savedUserEntity.getLogin());
+        assertThat(userEntityFromRepository.get().getRole()).isEqualTo(savedUserEntity.getRole());
     }
+
+    @Test
+    public void itShouldNotSaveUserNullableRole() {
+
+        UserEntity userEntity = UserEntity.builder()
+                .date(ZonedDateTime.now())
+                .firstName("Bulat")
+                .lastName("Zalyaliev")
+                .email("bulat@he.co")
+                .login("bulat")
+                .build();
+
+        assertThatThrownBy(() -> userRepository.saveAndFlush(userEntity))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+    @Test
+    public void itShouldNotSavesUserWithRoleThatDoesNotExistsInDatabase() {
+        RoleEntity roleEntityUser = RoleEntity.builder()
+                .name(Role.USER)
+                .build();
+
+        RoleEntity savedRoleEntityUser = roleRepository.save(roleEntityUser);
+
+        RoleEntity roleEntityAdmin = RoleEntity.builder()
+                .name(Role.ADMIN)
+                .build();
+
+        UserEntity userEntity = UserEntity.builder()
+                .date(ZonedDateTime.now())
+                .firstName("Bulat")
+                .lastName("Zalyaliev")
+                .email("bulat@he.co")
+                .login("bulat")
+                .role(roleEntityAdmin)
+                .build();
+
+        assertThatThrownBy(() -> userRepository.saveAndFlush(userEntity))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+
 
 }
